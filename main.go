0fa248ec47df25f79964bc3e2876211b85bcd1a9
tesla59 <nishant@heim.id>
2023-09-09 04:33:38 +0530

    main: move yaml parser to init()

--- main.go
+++ main.go
@@ -8,11 +8,7 @@ import (
 
 func init() {
 	BuildImage("./worker", "worker-image")
-}
 
-var config Config
-
-func main() {
 	yamlFile, err := os.ReadFile("config.yaml")
 	if err != nil {
 		panic(err)
@@ -20,4 +16,9 @@ func main() {
 	if err := yaml.Unmarshal(yamlFile, &config); err != nil {
 		panic(err)
 	}
+}
+
+var config Config
+
+func main() {
 }
