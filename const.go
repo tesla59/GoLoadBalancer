cc2a792b5fa56b9118c57a517efdfa276ec4ac3e
tesla59 <nishant@heim.id>
2023-09-09 15:42:28 +0530

    treewide: update db extension

--- const.go
+++ const.go
@@ -1,7 +1,7 @@
 package main
 
-const DatabaseTargetPath = "/app/worker-stats.sql"
-const DatabaseFileName = "worker-stats.sql"
+const DatabaseTargetPath = "/app/worker-stats.db"
+const DatabaseFileName = "worker-stats.db"
 const DefaultProtocol = "tcp"
 const DefaultWorkerImageTag = "worker-image"
 const DefaultConfigFilePath = "./config.yaml"
